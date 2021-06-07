45a19f054c7921d547850702a39b117eb1169081
Sean Wong <seanwong24@live.com>
2021-06-07 16:01:15 -0600

    rename services module to service module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ControllerModule } from './controller/controller.module';
 import { RepositoryModule } from './repository/repository.module';
-import { ServicesModule } from './services/services.module';
+import { ServiceModule } from './service/service.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://db'), // TODO move the connection string to a config file
     ControllerModule,
     RepositoryModule,
-    ServicesModule,
+    ServiceModule,
   ],
   controllers: [AppController],
   providers: [AppService],
