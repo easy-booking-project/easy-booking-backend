f660f6dd6f6fbcf0163330059fdeae9a43a59e65
Sean Wong <seanwong24@live.com>
2021-06-07 00:16:22 -0600

    add services module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ControllerModule } from './controller/controller.module';
 import { RepositoryModule } from './repository/repository.module';
+import { ServicesModule } from './services/services.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://db'), // TODO move the connection string to a config file
     ControllerModule,
-    RepositoryModule
+    RepositoryModule,
+    ServicesModule
   ],
   controllers: [AppController],
   providers: [AppService],
