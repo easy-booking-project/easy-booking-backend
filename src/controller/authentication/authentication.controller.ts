ce3aa18a5a847ed66778552878ddc549607954fd
Will Wei <will@rivercitytech.ca>
2021-06-08 23:11:08 -0600

    Complete single token auth and role guard
    Co-authored-by: Sean Wong <seanwong24@live.com>

--- src/controller/authentication/authentication.controller.ts
+++ src/controller/authentication/authentication.controller.ts
@@ -1,20 +1,30 @@
-import { Body, Controller, Post, Query } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { User } from '@repository/user/user.schema';
+import { AuthService } from '@service/auth/auth.service';
+import { JwtAuthGuard } from '@service/auth/jwt-auth.guard';
 
 @Controller('authentication')
 export class AuthenticationController {
+  constructor(private authService: AuthService) {}
+
   @Post('sign-up')
   async signUp(@Body() user: User) {
     return user;
   }
 
+  // TODO use Two token Auth
   @Post('sign-in')
   async signIn(@Body() info: { username: string; authenticationHash: string }) {
-    return info;
+    return this.authService.login({
+      username: info?.username || '',
+      authenticationHash: info?.authenticationHash || '',
+    } as User);
   }
 
+  // TODO use cookie instead of header
+  @UseGuards(JwtAuthGuard)
   @Post('sign-out')
-  async signOut(@Query() _id: string) {
-    return _id;
+  async signOut(@Req() req) {
+    return this.authService.logout(req.user);
   }
 }
