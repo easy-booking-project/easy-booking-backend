ceba93b7a0f92917a6a60eacb3b9d034160a359b
Will Wei <will@rivercitytech.ca>
2021-06-09 20:13:47 -0600

    rename repo service follow up

--- src/controller/user/user.controller.ts
+++ src/controller/user/user.controller.ts
@@ -12,25 +12,25 @@ import { UserRepository } from '@repository/user/user.repository';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserRepository) {}
+  constructor(private readonly userRepository: UserRepository) {}
 
   @Get()
   async obtain(@Query('_id') _id: string) {
-    return await this.userService.find(_id ? { _id } : {});
+    return await this.userRepository.find(_id ? { _id } : {});
   }
 
   @Post()
   async create(@Body() user: Partial<User>) {
-    return await this.userService.insert(user);
+    return await this.userRepository.insert(user);
   }
 
   @Put()
   async modify(@Query('_id') _id: string, @Body() user: Partial<User>) {
-    return this.userService.update({ _id }, user);
+    return this.userRepository.update({ _id }, user);
   }
 
   @Delete()
   async delete(@Query('_id') _id: string) {
-    return this.userService.remove({ _id });
+    return this.userRepository.remove({ _id });
   }
 }
