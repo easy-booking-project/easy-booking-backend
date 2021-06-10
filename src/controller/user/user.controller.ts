a38dfb68f8b1589ee239e59c6b7c69ce9d8daa34
Will Wei <will@rivercitytech.ca>
2021-06-10 09:26:01 -0600

    Change it so that user store role id as an array in database

--- src/controller/user/user.controller.ts
+++ src/controller/user/user.controller.ts
@@ -16,7 +16,6 @@ import { JwtAuthGuard } from '@service/auth/jwt-auth.guard';
 import { Roles } from '@service/auth/roles.decorator';
 import { RolesGuard } from '@service/auth/roles.guard';
 
-
 @Controller('user')
 export class UserController {
   constructor(private readonly userRepository: UserRepository) {}
