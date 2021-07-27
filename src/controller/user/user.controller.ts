8b7fa81143b91420893471cde08a87b10a0796fc
willwei954 <will@rivercitytech.ca>
2021-07-27 12:50:52 -0600

    user decorator

--- src/controller/user/user.controller.ts
+++ src/controller/user/user.controller.ts
@@ -11,39 +11,24 @@ import {
 } from '@nestjs/common';
 import { User } from '@repository/user/user.schema';
 import { UserRepository } from '@repository/user/user.repository';
-import { CookieKeys } from '@service/auth/constant';
 import { AllowRoles } from '@service/auth/roles.decorator';
+import { UserInfo } from '@service/auth/user.decorator';
 import { RolesGuard } from '@service/auth/roles.guard';
 import { JwtAuthGuard } from '../../service/auth/jwt-auth.guard';
 import { Request } from 'express';
 import { AuthService } from '@service/auth/auth.service';
 import { Roles } from '@repository/role/role.schema';
+import { UserEntity } from '../../repository/user/user.schema';
 
 @Controller('user')
 export class UserController {
-  constructor(
-    private readonly userRepository: UserRepository,
-    private readonly authService: AuthService,
-  ) {}
+  constructor(private readonly userRepository: UserRepository) {}
 
   @UseGuards(JwtAuthGuard, RolesGuard)
   @AllowRoles(Roles.Super, Roles.Admin, Roles.User)
   @Get('fetch')
   @Get('/obtain')
-  async obtain(@Req() request: Request) {
-    const accessToken = request.cookies[CookieKeys.ACCESS_TOKEN];
-
-    const decodedUserInfo = (await this.authService.jwtDecode(
-      accessToken,
-    )) as User;
-
-    const user = await this.userRepository.findOne(
-      decodedUserInfo._id ? { _id: decodedUserInfo._id } : {},
-    );
-
-    delete user.token;
-    delete user.authenticationHash;
-
+  async obtain(@Req() request: Request, @UserInfo() user: UserEntity) {
     return user;
   }
 
