4949829356acc4f7d3ed569f68d69d6c096e5a0c
willwei954 <will@rivercitytech.ca>
2021-08-05 15:09:24 -0600

    for now

--- src/controller/calendar/appointment.controller.ts
+++ src/controller/calendar/appointment.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AppointmentRepository } from '@repository/appointment/appointment.repository';
 import { Appointment } from '@repository/appointment/appointment.schema';
+import { JwtAuthGuard } from '@service/auth/jwt-auth.guard';
 
 @Controller('appointment')
 export class AppointmentController {
@@ -11,6 +12,7 @@ export class AppointmentController {
     return await this.appointmentRepository.find({});
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('/create')
   async create(@Body() appointment: Partial<Appointment>) {
     return await this.appointmentRepository.insert(appointment);
