1a473dfc8a09bae27cddff82b3e64817809154ba
willwei954 <will@rivercitytech.ca>
2021-08-06 11:36:52 -0600

    for now

--- src/controller/calendar/appointment.controller.ts
+++ src/controller/calendar/appointment.controller.ts
@@ -1,20 +1,36 @@
 import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AppointmentRepository } from '@repository/appointment/appointment.repository';
 import { Appointment } from '@repository/appointment/appointment.schema';
+import { UserEntity } from '@repository/user/user.schema';
 import { JwtAuthGuard } from '@service/auth/jwt-auth.guard';
+import { UserInfo } from '@service/auth/user.decorator';
 
 @Controller('appointment')
 export class AppointmentController {
   constructor(private readonly appointmentRepository: AppointmentRepository) {}
 
-  @Get('/fetch')
+  @Get('/fetchAll')
   async fetchAllAppointments() {
     return await this.appointmentRepository.find({});
   }
 
   @UseGuards(JwtAuthGuard)
+  @Get('/fetch')
+  async fetchAppointmentsById(@UserInfo() user: UserEntity) {
+    return await this.appointmentRepository.find({
+      participants: user.username,
+    });
+  }
+
+  @UseGuards(JwtAuthGuard)
   @Post('/create')
-  async create(@Body() appointment: Partial<Appointment>) {
+  async create(
+    @Body() appointment: Partial<Appointment>,
+    @UserInfo() user: UserEntity,
+  ) {
+    appointment.createdBy = user.id;
+    appointment.createdOn = new Date();
+
     return await this.appointmentRepository.insert(appointment);
   }
 }
--- src/controller/calendar/slot.controller.ts
+++ src/controller/calendar/slot.controller.ts
@@ -1,6 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { SlotRepository } from '@repository/slot/slot.repository';
 import { Slot } from '@repository/slot/slot.schema';
+import { Roles } from '@repository/role/role.schema';
+import { UserEntity } from '@repository/user/user.schema';
+import { JwtAuthGuard } from '@service/auth/jwt-auth.guard';
+import { AllowRoles } from '@service/auth/roles.decorator';
+import { RolesGuard } from '@service/auth/roles.guard';
+import { UserInfo } from '@service/auth/user.decorator';
 
 @Controller('slot')
 export class SlotController {
@@ -11,8 +17,13 @@ export class SlotController {
     return await this.slotRepository.find({});
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @AllowRoles(Roles.Super, Roles.Admin, Roles.Member)
   @Post('/create')
-  async create(@Body() slot: Partial<Slot>) {
+  async create(@Body() slot: Partial<Slot>, @UserInfo() user: UserEntity) {
+    slot.createdBy = user.id;
+    slot.createdOn = new Date();
+
     return await this.slotRepository.insert(slot);
   }
 }
